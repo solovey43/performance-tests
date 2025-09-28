0fa396e5b4311ccd930836344f351ebeb499b63b
Катерина <tsypinaea@gmail.com>
2025-09-28 13:37:53 +0300

    Практикуемся в использовании gRPC API клиентов

---
+++ grpc_api_client_make_top_up_operation.py
@@ -0,0 +1,34 @@
+from clients.grpc.gateway.accounts.client import build_accounts_gateway_grpc_client
+from clients.grpc.gateway.cards.client import build_cards_gateway_grpc_client
+from clients.grpc.gateway.users.client import build_users_gateway_grpc_client
+from clients.grpc.gateway.operations.client import build_operations_gateway_grpc_client
+
+# Инициализация клиентов
+users_gateway_client = build_users_gateway_grpc_client()
+cards_gateway_client = build_cards_gateway_grpc_client()
+accounts_gateway_client = build_accounts_gateway_grpc_client()
+operations_gateway_client = build_operations_gateway_grpc_client()
+
+# Создаем пользователя
+create_user_response = users_gateway_client.create_user()
+print('Create user response:', create_user_response)
+
+
+# Открываем дебетовый счет
+open_debit_card_account_response = accounts_gateway_client.open_debit_card_account(
+    user_id=create_user_response.user.id
+)
+print('Open debit card account response:', open_debit_card_account_response)
+
+
+# Получаем ID созданного счета  и карты
+account_id = open_debit_card_account_response.account.id
+card_id = open_debit_card_account_response.account.cards[0].id
+
+
+# Операция пополнения счета
+make_top_up_response = operations_gateway_client.make_top_up_operation(
+    card_id=card_id,
+    account_id=account_id
+)
+print('Make top up operation response:', make_top_up_response)
